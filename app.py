2d53dc43037ed99ad15ad2638a558744744ca4a9
Imad Hsissou <imad.hsissou@gmail.com>
2017-12-10 09:40:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def hello():
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Bonjour, bienvenu à Web 2.0 !</h3>" \
+    html = "<h3>Bonjour, Welcome to Web 2.0 !</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
